49aa289ade2ad02c8adb90eff223abc4cf7f8979
inlancersystem4 <inlancersystem4@gmail.com>
2023-12-16 17:41:36 +0530

    add all files

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            $('.sticky-header').addClass('bg-white/80 border-gray-200 backdrop-blur-lg py-2');
+            $('.sticky-header').removeClass('border-transparent py-5');
+        } else {
+            $('.sticky-header').removeClass('bg-white/80 border-gray-200 backdrop-blur-lg py-2');
+            $('.sticky-header').addClass('border-transparent py-5');
+        }
+    });
+
+
+    $(".custom-dropdown").click(function (event) {
+        event.stopPropagation();
+        $(this).children(".custom-dropdown").toggleClass("hidden");
+    });
+
+    $(document.body).click(function (event) {
+        var $target = $(event.target);
+        if (!$target.closest('.custom-dropdown').length) {
+            $('.custom-dropdown').children(".custom-dropdown").addClass('hidden');
+        }
+    })
+
+    $("#nav-menu").click(function () {
+        $(".toggle-nav").toggleClass("hidden")
+    })
+
+});
+
